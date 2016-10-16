2eb5fc9b8417edadcb9a0775a2ee4252cec19a59
Mohammed Hamood <m_alkhalifah88@yahoo.com>
2016-10-16 19:02:33 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+#=============
+  def name
+    @name
+  end
+#=============
+  def job
+@job
+  end
+#============
+  def job=(peson_job)
+@job = peson_job
+  end
+end
